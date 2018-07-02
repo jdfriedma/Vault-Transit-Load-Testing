fd4e64910ee510d43a81517285d367475e2e65e0
Jacob Friedman <jacob@hashicorp.com>
2018-07-02 16:32:41 -0700

    Create single-key-post.lua

---
+++ single-key-post.lua
@@ -0,0 +1,8 @@
+
+-- example HTTP POST script which demonstrates setting the
+-- HTTP method, body, and adding a header
+
+wrk.method = "POST"
+wrk.body   = '{"plaintext" : "dGVldkV0R2ZodVV5Yk9zUA=="}'
+wrk.headers["Content-Type"] = "application/json"
+ubuntu@ip-10-0-0-20:~$
